7b2939acf7b15b331131f84bf4afce7137fcb584
Jonathan Carlson <Car99020@byui.edu>
2019-07-08 18:35:10 -0500

    Uploading portfolio folders

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="resume" content="width=device-width, initial-scale=1">
+        <title>Jonathan Carlson's Portfolio</title>
+        <meta name = "description" content="Software engineering portfolio for Jonathan Carlson of Huntsville, Alabama.">
+        <link rel="stylesheet" href="styles/normalize.css">
+        <link id="styles" rel="stylesheet" href="styles/main.css">
+    </head>
+
+    <body>
+
+        <nav>
+            <!--A main menu is comming in the future!-->
+        </nav>
+
+        <header>
+            <h1>Jonathan Carlson</h1>
+        </header>
+
+        <main>
+
+            <div class="row">
+                <div class="column" id="column1">
+                    <img src="images/jon.jpg" alt="Profile picture">
+                    <h2>Software Engineer</h2>
+                    <div class="line"></div>
+                    <h3>Contact:</h3>
+                    <p class="contact">705 Larry Drive<br> Madison, Al 35758<br><br>
+                    H: 256-513-6210<br>
+                    C: 256-665-8512<br>
+                    O: 256-289-1177</p><br>
+                    <p class="contact">
+                        Work:<br>
+                        <a href="mailto:Jonathan.C.Carlson@SSA.gov?Subject=Position%20Vacancy" target="_top">Jonathan.C.Carlson@SSA.gov</a><br><br>
+                    
+                        Academic:<br>
+                        <a href="mailto:Car99020@byui.edu?Subject=Position%20Vacancy" target="_top">Car99020@BYUI.edu</a><br><br>
+                    
+                        Personal:
+                        <a href="mailto:JonathanCarlson3712@hotmail.com?Subject=Position%20Vacancy" target="_top">JonathanCarlson3712@Hotmail.com</a>
+                        <span class="detail"> (preferred method of contact)</span></p><br><br>
+                </div>
+
+                <div class="column" id="column2">
+                    <h3>Professional Credentials</h3>
+
+                    <ul>
+                        <a href="images/Jonathan%20Carlson's%20Resume.pdf"/a><li>Resume</li></a>
+                        <a href="images/Letter%20of%20Recommendation%20-%20Martin%20Roggio.pdf"><li>Letter of Recommendation</li></a>
+                    </ul>
+                    <h3>Coding Projects</h3>
+                    <ul>
+                        <a href=""><li>Serpentine (Python)</li></a>
+                        <a href=""><li>Labyrinthia (Python)</li></a>
+                        <a href=""><li>Pantry App (Java)</li></a>  
+                        <a href=""><li>Data Types (C++)</li></a>  
+                        <a href="pages/sra.html"><li>Front-End Web Design (HTML, CSS, JavaScript)</li></a>
+                        <a href=""><li>JS DOM manipulation (JavaScript)</li></a>  
+                        <a href=""><li>Database Design (PostgreSQL, PHP)</li></a>   
+                        <a href=""><li>API Feeds (Node.js, AJAX)</li></a>   
+                        <a href=""><li>Rocket City Rebels</li></a>  
+                    </ul>
+            
+                    <h3>Other</h3>
+                    <ul>
+                        <a href="pages/processor.html"><li>Digital Micro-Processor</li></a>
+                        <a href=""><li>Social Security Benefits Calculator</li></a>
+                        <a href=""><li>Social Security LAF Calculator</li></a>
+                        <a href=""><li>Software Requirements Specification (SRS)</li></a>
+                        <a href=""><li>Software Design Description (SDD)</li></a>
+                    </ul>
+                </div>
+            </div>
+
+        </main>  
+
+        <footer>            
+            <p>&copy; 2019 &#9670; Jonathan Carlson &#9670; Madison Alabama</p>
+            <?php echo date("l, F d, Y - h:ia"); ?>
+        </footer>
+    </body>
+</html>
